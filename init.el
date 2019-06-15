b5d082baf2269ed58233f37ff8bee927f9c90841
Lomov Konstantin <lomov.konstantin@gmail.com>
2019-06-15 22:18:23 +0500

    Update init.el

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+GNU Emacs configuration file
+
 ;; Show-paren-mode settings
 (show-paren-mode t) ;; включить выделение выражений между {},[],()
 (setq show-paren-style 'expression) ;; выделить цветом выражения между {},[],()
