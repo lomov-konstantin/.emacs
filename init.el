81e5ce65831bcdebad41a05e060265d4b9695b9a
Lomov Konstantin <lomov.konstantin@gmail.com>
2019-06-17 08:59:22 +0500

    Update init.el

--- init.el
+++ init.el
@@ -24,11 +24,6 @@ GNU Emacs configuration file
 (when (window-system)
     (set-frame-size (selected-frame) 150 50))
 
-;; Display file size/time in mode-line
-(setq display-time-24hr-format t) ;; 24-часовой временной формат в mode-line
-(display-time-mode             t) ;; показывать часы в mode-line
-(size-indication-mode          t) ;; размер файла в %-ах
-
 (load-theme 'deeper-blue)
 (setq frame-title-format "Emacs")
 (setq redisplay-dont-pause t)  ;; лучшая отрисовка буфера
